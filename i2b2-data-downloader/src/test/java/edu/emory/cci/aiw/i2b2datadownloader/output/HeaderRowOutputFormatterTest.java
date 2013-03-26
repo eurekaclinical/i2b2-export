a3f7056d86b542ef4a1dcbb53c06a42b5ba97d61
Michel Mansour <michel.mansour@emory.edu>
2013-03-26 21:53:23 +0000

    added test dir

---
+++ i2b2-data-downloader/src/test/java/edu/emory/cci/aiw/i2b2datadownloader/output/HeaderRowOutputFormatterTest.java
@@ -0,0 +1,14 @@
+package edu.emory.cci.aiw.i2b2datadownloader.output;
+
+import org.junit.Test;
+
+/**
+ *
+ */
+public class HeaderRowOutputFormatterTest {
+
+	@Test
+	public void testFormatHeader() {
+		
+	}
+}
