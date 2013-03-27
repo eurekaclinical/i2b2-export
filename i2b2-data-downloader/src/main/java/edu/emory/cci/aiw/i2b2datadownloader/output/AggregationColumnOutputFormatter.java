cd42ac12ce3e3dd1ddce40de4b154033ac104198
Michel Mansour <michel.mansour@emory.edu>
2013-03-27 21:57:17 +0000

    changed format() to accept a collection of observations

--- i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/AggregationColumnOutputFormatter.java
+++ i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/AggregationColumnOutputFormatter.java
@@ -1,18 +1,21 @@
 package edu.emory.cci.aiw.i2b2datadownloader.output;
 
+import edu.emory.cci.aiw.i2b2datadownloader.entity.OutputColumnConfiguration;
 import edu.emory.cci.aiw.i2b2datadownloader.i2b2.pdo.Observation;
+
+import java.util.Collection;
 
 /**
  *
  */
 public class AggregationColumnOutputFormatter extends AbstractColumnOutputFormatter {
 
-	public AggregationColumnOutputFormatter(FormatOptions formatOptions) {
-		super(formatOptions);
+	public AggregationColumnOutputFormatter(OutputColumnConfiguration columnConfig, FormatOptions formatOptions) {
+		super(columnConfig, formatOptions);
 	}
 
 	@Override
-	public String format(Observation data) {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
-	}
+	public String format(Collection<Observation> data) {
+		return null;
+    }
 }
