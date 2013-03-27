f33a0a135a90d5cb146c172b6598d786e4b15a81
Michel Mansour <michel.mansour@emory.edu>
2013-03-27 22:10:03 +0000

    started implementation of format

--- i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/AggregationColumnOutputFormatter.java
+++ i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/AggregationColumnOutputFormatter.java
@@ -3,7 +3,9 @@ package edu.emory.cci.aiw.i2b2datadownloader.output;
 import edu.emory.cci.aiw.i2b2datadownloader.entity.OutputColumnConfiguration;
 import edu.emory.cci.aiw.i2b2datadownloader.i2b2.pdo.Observation;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  *
@@ -16,6 +18,25 @@ public class AggregationColumnOutputFormatter extends AbstractColumnOutputFormat
 
 	@Override
 	public String format(Collection<Observation> data) {
+        List<String> result = new ArrayList<String>();
+
+        if (data == null || data.isEmpty()) {
+            result.add(getFormatOptions().getMissingData());
+            if (getColumnConfig().getIncludeUnits()) {
+                result.add(getFormatOptions().getMissingData());
+            }
+        }
+        switch (getColumnConfig().getAggregation()) {
+            case MIN:
+                break;
+            case MAX:
+                break;
+            case AVG:
+                break;
+            default:
+                throw new RuntimeException("aggregation type not provided");
+        }
+
 		return null;
     }
 }
