38a4b96e6e1a92ce405d05383854516c3410b13d
Michel Mansour <michel.mansour@emory.edu>
2013-03-27 21:56:54 +0000

    changed format() to accept a collection of observations

--- i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/AbstractColumnOutputFormatter.java
+++ i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/AbstractColumnOutputFormatter.java
@@ -1,15 +1,23 @@
 package edu.emory.cci.aiw.i2b2datadownloader.output;
 
+import edu.emory.cci.aiw.i2b2datadownloader.entity.OutputColumnConfiguration;
+
 /**
  *
  */
 public abstract class AbstractColumnOutputFormatter implements
 		ColumnOutputFormatter {
+    private final OutputColumnConfiguration columnConfig;
 	private final FormatOptions formatOptions;
 
-	protected AbstractColumnOutputFormatter(FormatOptions formatOptions) {
+	protected AbstractColumnOutputFormatter(OutputColumnConfiguration columnConfig, FormatOptions formatOptions) {
+        this.columnConfig = columnConfig;
 		this.formatOptions = formatOptions;
 	}
+
+    protected OutputColumnConfiguration getColumnConfig() {
+        return columnConfig;
+    }
 
 	protected FormatOptions getFormatOptions() {
 		return formatOptions;
--- i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/ColumnOutputFormatter.java
+++ i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/ColumnOutputFormatter.java
@@ -2,15 +2,19 @@ package edu.emory.cci.aiw.i2b2datadownloader.output;
 
 import edu.emory.cci.aiw.i2b2datadownloader.i2b2.pdo.Observation;
 
+import java.util.Collection;
+
 /**
  *
  */
 public interface ColumnOutputFormatter {
 
-	/**
-	 * Generates output for patient data based on a column configuration
-	 *
-	 * @return The output
-	 */
-	public String format(Observation data);
+    /**
+     * Generates output for patient data based on a column configuration.
+     *
+     * @param data The observations to format. This collection should include all of the observations matching a particular i2b2 concept.
+     *
+     * @return The formatted output
+     */
+    public String format(Collection<Observation> data);
 }
