28c4fd98bc63d592e9f0919c6b042ca8c8d2e868
Michel Mansour <michel.mansour@emory.edu>
2013-03-27 21:56:19 +0000

    added missing data field

--- i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/FormatOptions.java
+++ i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/FormatOptions.java
@@ -1,17 +1,25 @@
 package edu.emory.cci.aiw.i2b2datadownloader.output;
 
+import edu.emory.cci.aiw.i2b2datadownloader.entity.OutputConfiguration;
+
 /**
  *
  */
 public final class FormatOptions {
 	private final String columnSeparator;
+    private final String missingData;
 
-	public FormatOptions(String columnSeparator,
-						 String whitespaceReplacement) {
-		this.columnSeparator = columnSeparator;
+	public FormatOptions(OutputConfiguration config) {
+		this.columnSeparator = config.getSeparator();
+        this.missingData = config.getMissingValue();
 	}
 
 	public String getColumnSeparator() {
 		return columnSeparator;
 	}
+
+    public String getMissingData() {
+        return missingData;
+    }
+
 }
