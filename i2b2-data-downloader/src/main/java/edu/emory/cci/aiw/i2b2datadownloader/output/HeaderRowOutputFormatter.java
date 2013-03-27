e3daae8db7823f83bbb2b44fa35e1f02de444654
Michel Mansour <michel.mansour@emory.edu>
2013-03-27 21:58:11 +0000

    added code for row dimension column header

--- i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/HeaderRowOutputFormatter.java
+++ i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/HeaderRowOutputFormatter.java
@@ -12,59 +12,71 @@ import java.util.List;
  *
  */
 public class HeaderRowOutputFormatter {
-	private final OutputConfiguration outputConfiguration;
+    private final OutputConfiguration outputConfiguration;
 
-	HeaderRowOutputFormatter(OutputConfiguration outputConfiguration) {
-		this.outputConfiguration = outputConfiguration;
-	}
+    HeaderRowOutputFormatter(OutputConfiguration outputConfiguration) {
+        this.outputConfiguration = outputConfiguration;
+    }
 
-	public String formatHeader() {
-		List<String> result = new ArrayList<String>();
-		Collections.sort(outputConfiguration.getColumnConfigs());
-		for (int i = 0; i < outputConfiguration.getColumnConfigs().size() -
-				1; i++) {
-			OutputColumnConfiguration colConfig = outputConfiguration
-					.getColumnConfigs().get(i);
-			String baseColName = colConfig.getColumnName().replaceAll("\\s",
-					outputConfiguration.getWhitespaceReplacement());
-			switch (colConfig.getDisplayFormat()) {
-				case EXISTENCE:
-					result.add(baseColName);
-					break;
-				case VALUE:
-					for (int j = 0; j < colConfig.getHowMany(); j++) {
-						result.add(baseColName + "_value");
-						if (colConfig.getIncludeUnits()) {
-							result.add(baseColName + "_units");
-						}
-						if (colConfig.getIncludeTimeRange()) {
-							result.add(baseColName + "_start");
-							result.add(baseColName + "_end");
-						}
-					}
-					break;
-				case AGGREGATION:
-					switch (colConfig.getAggregation()) {
-						case MIN:
-							result.add(baseColName + "_min");
-							break;
-						case MAX:
-							result.add(baseColName + "_max");
-							break;
-						case AVG:
-							result.add(baseColName + "_avg");
-							break;
-						default:
-							continue;
-					}
-					if (colConfig.getIncludeUnits()) {
-						result.add(baseColName + "_units");
-					}
-					break;
-				default:
-					break;
-			}
-		}
-		return StringUtils.join(result, outputConfiguration.getSeparator());
-	}
+    public String formatHeader() {
+        List<String> result = new ArrayList<String>();
+        switch (outputConfiguration.getRowDimension()) {
+            case PATIENT:
+                result.add("Patient_id");
+                break;
+            case VISIT:
+                result.add("Visit_id");
+                break;
+            case PROVIDER:
+                result.add("Provider_id");
+                break;
+            default:
+                throw new RuntimeException("row dimension not provided: userId: " + outputConfiguration.getUserId() + ", name: " + outputConfiguration.getName());
+        }
+        Collections.sort(outputConfiguration.getColumnConfigs());
+        for (int i = 0; i < outputConfiguration.getColumnConfigs().size(); i++) {
+            OutputColumnConfiguration colConfig = outputConfiguration
+                    .getColumnConfigs().get(i);
+            String baseColName = colConfig.getColumnName().replaceAll("\\s",
+                    outputConfiguration.getWhitespaceReplacement());
+            switch (colConfig.getDisplayFormat()) {
+                case EXISTENCE:
+                    result.add(baseColName);
+                    break;
+                case VALUE:
+                    for (int j = 0; j < colConfig.getHowMany(); j++) {
+                        result.add(baseColName + "_value");
+                        if (colConfig.getIncludeUnits()) {
+                            result.add(baseColName + "_units");
+                        }
+                        if (colConfig.getIncludeTimeRange()) {
+                            result.add(baseColName + "_start");
+                            result.add(baseColName + "_end");
+                        }
+                    }
+                    break;
+                case AGGREGATION:
+                    switch (colConfig.getAggregation()) {
+                        case MIN:
+                            result.add(baseColName + "_min");
+                            break;
+                        case MAX:
+                            result.add(baseColName + "_max");
+                            break;
+                        case AVG:
+                            result.add(baseColName + "_avg");
+                            break;
+                        default:
+                            continue;
+                    }
+                    if (colConfig.getIncludeUnits()) {
+                        result.add(baseColName + "_units");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return StringUtils.join(result, outputConfiguration.getSeparator());
+    }
 }
