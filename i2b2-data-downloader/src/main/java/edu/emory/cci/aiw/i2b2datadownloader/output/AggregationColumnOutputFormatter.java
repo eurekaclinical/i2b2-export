1e2176cbd4377ff46282e0dc8afb1dbb847d1452
Michel Mansour <michel.mansour@emory.edu>
2013-03-28 21:28:51 +0000

    made class final

--- i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/AggregationColumnOutputFormatter.java
+++ i2b2-data-downloader/src/main/java/edu/emory/cci/aiw/i2b2datadownloader/output/AggregationColumnOutputFormatter.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class AggregationColumnOutputFormatter extends AbstractColumnOutputFormatter {
+public final class AggregationColumnOutputFormatter extends AbstractColumnOutputFormatter {
 
 	public AggregationColumnOutputFormatter(OutputColumnConfiguration columnConfig, FormatOptions formatOptions) {
 		super(columnConfig, formatOptions);
