4586d693952bb55baaa635297798bbee6da1007c
Michel Mansour <michel.mansour@emory.edu>
2013-04-23 22:09:10 +0000

    uncommented previously erroneous tests that now succeed due to changes in the mock i2b2 PDO retriever

--- i2b2-patient-data-export-service/src/test/java/edu/emory/cci/aiw/i2b2patientdataexport/resource/DataResourceTest.java
+++ i2b2-patient-data-export-service/src/test/java/edu/emory/cci/aiw/i2b2patientdataexport/resource/DataResourceTest.java
@@ -44,28 +44,28 @@ public class DataResourceTest extends AbstractResourceTest {
 
 	@Test
 	public void testExportConfigIdOk() {
-//		ClientResponse response = this.resource().path
-//				("/rest/export/configId").queryParam("i2b2-domain",
-//				"test-domain").queryParam
-//						("i2b2-user", "test-user").queryParam("i2b2-pass",
-//						"test-pass").queryParam("i2b2-project",
-//						"test-project").queryParam("config-id",
-//						"1").queryParam("patient-set-coll-id",
-//						"1").queryParam("patient-set-size",
-//						"10").accept("text/csv").type(MediaType
-//				.APPLICATION_FORM_URLENCODED_TYPE).post(ClientResponse
-//				.class);
-//
-//		Assert.assertEquals(ClientResponse.Status.OK, response.getClientResponseStatus());
+		ClientResponse response = this.resource().path
+				("/rest/export/configId").queryParam("i2b2-domain",
+				"test-domain").queryParam
+						("i2b2-user", "test-user").queryParam("i2b2-pass",
+						"test-pass").queryParam("i2b2-project",
+						"test-project").queryParam("config-id",
+						"1").queryParam("patient-set-coll-id",
+						"1").queryParam("patient-set-size",
+						"10").accept("text/csv").type(MediaType
+				.APPLICATION_FORM_URLENCODED_TYPE).post(ClientResponse
+				.class);
+
+		Assert.assertEquals(ClientResponse.Status.OK, response.getClientResponseStatus());
 	}
 
 	@Test
 	public void testPreviewBadRequest() {
-//		ClientResponse response = this.resource().path
-//				("/rest/export/preview").entity(null).type(MediaType
-//				.APPLICATION_JSON_TYPE).post(ClientResponse.class);
-//
-//		Assert.assertEquals(ClientResponse.Status.INTERNAL_SERVER_ERROR,
-//				response.getClientResponseStatus());
+		ClientResponse response = this.resource().path
+				("/rest/export/preview").entity(null).type(MediaType
+				.APPLICATION_JSON_TYPE).post(ClientResponse.class);
+
+		Assert.assertEquals(ClientResponse.Status.INTERNAL_SERVER_ERROR,
+				response.getClientResponseStatus());
 	}
 }
