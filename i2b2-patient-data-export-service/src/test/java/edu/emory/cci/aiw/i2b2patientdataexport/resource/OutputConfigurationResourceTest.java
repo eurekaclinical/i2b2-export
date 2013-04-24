804c21ee3ec43405e92b0f8607d8e60dfa7221ec
Michel Mansour <michel.mansour@emory.edu>
2013-04-24 01:46:27 +0000

    added delete unit tests

--- i2b2-patient-data-export-service/src/test/java/edu/emory/cci/aiw/i2b2patientdataexport/resource/OutputConfigurationResourceTest.java
+++ i2b2-patient-data-export-service/src/test/java/edu/emory/cci/aiw/i2b2patientdataexport/resource/OutputConfigurationResourceTest.java
@@ -1,6 +1,7 @@
 package edu.emory.cci.aiw.i2b2patientdataexport.resource;
 
 import com.sun.jersey.api.client.ClientResponse;
+import edu.emory.cci.aiw.i2b2patientdataexport.comm.DeleteRequest;
 import edu.emory.cci.aiw.i2b2patientdataexport.comm.I2b2AuthMetadata;
 import edu.emory.cci.aiw.i2b2patientdataexport.comm.LoadRequest;
 import junit.framework.Assert;
@@ -133,5 +134,81 @@ public class OutputConfigurationResourceTest extends AbstractResourceTest {
 						.class);
 
 		Assert.assertEquals(1, response.getEntity(List.class).size());
+	}
+
+	@Test
+	public void testDeleteOk() {
+		DeleteRequest request = new DeleteRequest();
+		I2b2AuthMetadata authMetadata = new I2b2AuthMetadata();
+		authMetadata.setDomain("test-domain");
+		authMetadata.setUsername("test-user");
+		authMetadata.setPasswordNode("<password>test-password</password>");
+		authMetadata.setProjectId("test-project");
+		request.setAuthMetadata(authMetadata);
+		request.setOutputConfigurationId(1L);
+
+		ClientResponse response = this.resource().path("/rest/config/delete")
+				.entity(request).accept(MediaType.APPLICATION_JSON_TYPE).type
+						(MediaType.APPLICATION_JSON_TYPE).post(ClientResponse
+						.class);
+
+		Assert.assertEquals(ClientResponse.Status.OK, response.getClientResponseStatus());
+	}
+
+	@Test
+	public void testDeleteNotAuthorized() {
+		DeleteRequest request = new DeleteRequest();
+		I2b2AuthMetadata authMetadata = new I2b2AuthMetadata();
+		authMetadata.setDomain("test-domain");
+		authMetadata.setUsername("bad-user");
+		authMetadata.setPasswordNode("<password>test-password</password>");
+		authMetadata.setProjectId("test-project");
+		request.setAuthMetadata(authMetadata);
+		request.setOutputConfigurationId(1L);
+
+		ClientResponse response = this.resource().path("/rest/config/delete")
+				.entity(request).accept(MediaType.APPLICATION_JSON_TYPE).type
+						(MediaType.APPLICATION_JSON_TYPE).post(ClientResponse
+						.class);
+
+		Assert.assertEquals(ClientResponse.Status.UNAUTHORIZED, response.getClientResponseStatus());
+	}
+
+	@Test
+	public void testDeleteNotFound() {
+		DeleteRequest request = new DeleteRequest();
+		I2b2AuthMetadata authMetadata = new I2b2AuthMetadata();
+		authMetadata.setDomain("test-domain");
+		authMetadata.setUsername("test-user");
+		authMetadata.setPasswordNode("<password>test-password</password>");
+		authMetadata.setProjectId("test-project");
+		request.setAuthMetadata(authMetadata);
+		request.setOutputConfigurationId(-1L);
+
+		ClientResponse response = this.resource().path("/rest/config/delete")
+				.entity(request).accept(MediaType.APPLICATION_JSON_TYPE).type
+						(MediaType.APPLICATION_JSON_TYPE).post(ClientResponse
+						.class);
+
+		Assert.assertEquals(ClientResponse.Status.NOT_FOUND, response.getClientResponseStatus());
+	}
+
+	@Test
+	public void testDeleteWrongUsername() {
+		DeleteRequest request = new DeleteRequest();
+		I2b2AuthMetadata authMetadata = new I2b2AuthMetadata();
+		authMetadata.setDomain("test-domain");
+		authMetadata.setUsername("test-user");
+		authMetadata.setPasswordNode("<password>test-password</password>");
+		authMetadata.setProjectId("test-project");
+		request.setAuthMetadata(authMetadata);
+		request.setOutputConfigurationId(0L);
+
+		ClientResponse response = this.resource().path("/rest/config/delete")
+				.entity(request).accept(MediaType.APPLICATION_JSON_TYPE).type
+						(MediaType.APPLICATION_JSON_TYPE).post(ClientResponse
+						.class);
+
+		Assert.assertEquals(ClientResponse.Status.UNAUTHORIZED, response.getClientResponseStatus());
 	}
 }
