eb78c79713303023361561ee3fd0b65f204ab390
Michel Mansour <michel.mansour@emory.edu>
2013-11-13 21:01:05 +0000

    made the i2b2 proxy url and service host url externally configurable via a properties file; changd indentation to tabs

--- i2b2-patient-data-export-service/src/main/java/edu/emory/cci/aiw/i2b2patientdataexport/i2b2/I2b2CommUtil.java
+++ i2b2-patient-data-export-service/src/main/java/edu/emory/cci/aiw/i2b2patientdataexport/i2b2/I2b2CommUtil.java
@@ -15,21 +15,32 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
+import java.util.Properties;
 
 public final class I2b2CommUtil {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(I2b2CommUtil.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(I2b2CommUtil.class);
 
-    private static final String I2B2_PROXY_URL = "https://eureka-dev.cci.emory.edu/i2b2/index.php";
-    static final String I2B2_SERVICE_HOST_URL = "http://localhost:9090";
+	private static final String DEFAULT_PROPERTIES_FILE = "i2b2export.properties";
+	private static final String DEFAULT_PROPERTIES_PATH = "/etc/i2b2export/" + DEFAULT_PROPERTIES_FILE;
+	private static final String PROPERTIES_FILE_PROPERTY = "i2b2export.propertiesFile";
+	private static final String I2B2_PROXY_URL_PROPERTY = "i2b2ProxyUrl";
+	private static final String I2B2_SERVICE_HOST_URL_PROPERTY = "i2b2ServiceHostUrl";
+
+	private static String i2b2ProxyUrl = "";
+	private static String i2b2ServiceHostUrl = "";
 
 //    private static final String I2B2_PROXY_URL = "http://192.168.86.128/webclient/index.php";
 //    static final String I2B2_SERVICE_HOST_URL = "http://localhost:9090";
 
-    static final String TEMPLATES_DIR = "i2b2-xml-templates";
+	static final String TEMPLATES_DIR = "i2b2-xml-templates";
 
 	/*
 	 * The i2b2 date format. The colon (:) must be stripped out of the timezone portion first.
@@ -37,11 +48,81 @@ public final class I2b2CommUtil {
 	public static final String I2B2_DATE_FMT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
 	/**
+	 * Private constructor to prevent instantiation
+	 */
+	private I2b2CommUtil() {
+	}
+
+	private static void readProperties() {
+		InputStream propFileStream = null;
+		try {
+			if (System.getProperties().containsKey(PROPERTIES_FILE_PROPERTY) && new File(System.getProperty(PROPERTIES_FILE_PROPERTY)).exists()) {
+				LOGGER.info("Using system property for application properties file: {}", System.getProperty(PROPERTIES_FILE_PROPERTY));
+				propFileStream = new FileInputStream(System.getProperty(PROPERTIES_FILE_PROPERTY));
+			} else if (new File(DEFAULT_PROPERTIES_PATH).exists()) {
+				if (System.getProperties().containsKey(PROPERTIES_FILE_PROPERTY)) {
+					LOGGER.warn("Specified properties file not found: {}", System.getProperty(PROPERTIES_FILE_PROPERTY));
+				}
+				LOGGER.info("Using default location for application properties file: {}", DEFAULT_PROPERTIES_PATH);
+				propFileStream = new FileInputStream(DEFAULT_PROPERTIES_PATH);
+			} else {
+				if (System.getProperties().containsKey(PROPERTIES_FILE_PROPERTY)) {
+					LOGGER.warn("Specified properties file not found: {}", System.getProperty(PROPERTIES_FILE_PROPERTY));
+					LOGGER.info("Application properties file not found in default location: {}", DEFAULT_PROPERTIES_PATH);
+				}
+				LOGGER.info("Using classpath for application properties file");
+				propFileStream = I2b2CommUtil.class.getResourceAsStream("/" + DEFAULT_PROPERTIES_FILE);
+
+				if (null == propFileStream) {
+					LOGGER.warn("Could not find properties file in classpath: {}", DEFAULT_PROPERTIES_FILE);
+					LOGGER.error("Could not find properties file in system property {}, default location {}, or classpath!! Service will not work properly!!", PROPERTIES_FILE_PROPERTY, DEFAULT_PROPERTIES_PATH);
+				}
+			}
+			if (null != propFileStream) {
+				Properties p = new Properties();
+				p.load(propFileStream);
+				if (!p.containsKey(I2B2_PROXY_URL_PROPERTY) || !p.containsKey(I2B2_SERVICE_HOST_URL_PROPERTY)) {
+					LOGGER.error("Could not find required property in properties file. Service will not work properly!! Make sure the following properties are defined: {}, {}", I2B2_PROXY_URL_PROPERTY, I2B2_SERVICE_HOST_URL_PROPERTY);
+				} else {
+					i2b2ProxyUrl = p.getProperty(I2B2_PROXY_URL_PROPERTY);
+					i2b2ServiceHostUrl = p.getProperty(I2B2_SERVICE_HOST_URL_PROPERTY);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			LOGGER.error(e.getMessage(), e);
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage(), e);
+		} finally {
+			if (null != propFileStream) {
+				try {
+					propFileStream.close();
+				} catch (Exception e) {
+					LOGGER.error(e.getMessage(), e);
+				}
+			}
+		}
+	}
+
+	public static String getProxyUrl() {
+		if (null == i2b2ProxyUrl || i2b2ProxyUrl.isEmpty()) {
+			readProperties();
+		}
+		return i2b2ProxyUrl;
+	}
+
+	public static String getI2b2ServiceHostUrl() {
+		if (null == i2b2ServiceHostUrl || i2b2ServiceHostUrl.isEmpty()) {
+			readProperties();
+		}
+		return i2b2ServiceHostUrl;
+	}
+
+	/**
 	 * Performs an HTTP POST of an XML request to the i2b2 proxy cell. Request XML must contain
-     * the redirect_url node with the address of the actual service endpoint to call
-     * in order for the request to be properly routed.
+	 * the redirect_url node with the address of the actual service endpoint to call
+	 * in order for the request to be properly routed.
 	 *
-     * @param xml the XML request to send
+	 * @param xml the XML request to send
 	 * @return the XML response from i2b2 as a {@link Document}
 	 * @throws ClientProtocolException      if an error occurs
 	 * @throws IOException                  if an error occurs
@@ -52,28 +133,28 @@ public final class I2b2CommUtil {
 	public static Document postXmlToI2b2(String xml)
 			throws ClientProtocolException, IOException, IllegalStateException,
 			SAXException, ParserConfigurationException {
-        LOGGER.debug("POSTing XML: " + xml);
+		LOGGER.debug("POSTing XML: {}", xml);
 		HttpClient http = new DefaultHttpClient();
-		HttpPost i2b2Post = new HttpPost(I2B2_PROXY_URL);
+		HttpPost i2b2Post = new HttpPost(getProxyUrl());
 		StringEntity xmlEntity = new StringEntity(xml);
 		xmlEntity.setContentType("text/xml");
 		i2b2Post.setEntity(xmlEntity);
 		HttpResponse resp = http.execute(i2b2Post);
 
-        if (LOGGER.isDebugEnabled()) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
-            StringBuilder respXml = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                respXml.append(line);
-            }
-            LOGGER.debug("Response XML: " + respXml.toString());
+		if (LOGGER.isDebugEnabled()) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+			StringBuilder respXml = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				respXml.append(line);
+			}
+			LOGGER.debug("Response XML: {}", respXml.toString());
 
-            return XmlUtil.xmlStringToDocument(respXml.toString());
-        } else {
-            return DocumentBuilderFactory.newInstance().newDocumentBuilder()
-                    .parse(resp.getEntity().getContent());
-        }
+			return XmlUtil.xmlStringToDocument(respXml.toString());
+		} else {
+			return DocumentBuilderFactory.newInstance().newDocumentBuilder()
+					.parse(resp.getEntity().getContent());
+		}
 	}
 
 	/**
