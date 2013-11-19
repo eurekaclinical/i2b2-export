02fe7a09c87b2663d9b91b07952a80d73e12e890
Michel Mansour <michel.mansour@emory.edu>
2013-11-19 21:22:50 +0000

    added javadoc

--- i2b2-export-service/src/main/java/edu/emory/cci/aiw/i2b2export/entity/OutputColumnConfiguration.java
+++ i2b2-export-service/src/main/java/edu/emory/cci/aiw/i2b2export/entity/OutputColumnConfiguration.java
@@ -34,6 +34,11 @@ import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+/**
+ * A JPA entity representing an output column configuration.
+ *
+ * @author Michel Mansour
+ */
 @Entity
 @Table(name = "column_configs")
 public class OutputColumnConfiguration implements
@@ -61,8 +66,24 @@ public class OutputColumnConfiguration implements
 	@Column(name = "column_name", nullable = false)
 	private String columnName;
 
+	/**
+	 * Valid display formats for a column configuration
+	 */
 	public static enum DisplayFormat {
-		EXISTENCE, VALUE, AGGREGATION;
+		/**
+		 * whether data exists for this concept
+		 */
+		EXISTENCE,
+
+		/**
+		 * the value of the data for this concept
+		 */
+		VALUE,
+
+		/**
+		 * an aggregation of all of the data for this concept
+		 */
+		AGGREGATION;
 	}
 
 	@Column(name = "display_format", nullable = false)
@@ -77,87 +98,198 @@ public class OutputColumnConfiguration implements
 	@Column(name = "include_time_range")
 	private Boolean includeTimeRange;
 
+	/**
+	 * Valid aggregation types.
+	 */
 	public static enum AggregationType {
-		MIN, MAX, AVG;
+		/**
+		 * the minimum value across all data for this concept
+		 */
+		MIN,
+
+		/**
+		 * the maximum value across all data for this concept
+		 */
+		MAX,
+
+		/**
+		 * the average value of data for this concept
+		 */
+		AVG;
 	}
 
 	@Column(name = "aggregation")
 	private AggregationType aggregation;
 
+	/**
+	 * Gets the record's ID.
+	 *
+	 * @return the id
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * Gets the output configuration this column configuration belongs to.
+	 *
+	 * @return the {@link OutputConfiguration} this column configuration belongs to
+	 */
 	@JsonBackReference
 	public OutputConfiguration getOutputConfig() {
 		return outputConfig;
 	}
 
+	/**
+	 * Sets the output configuration this column configuration belongs to.
+	 *
+	 * @param outputConfig the output configuration
+	 */
 	public void setOutputConfig(OutputConfiguration outputConfig) {
 		this.outputConfig = outputConfig;
 	}
 
+	/**
+	 * Gets the order of this column's placement in the output.
+	 *
+	 * @return the column order
+	 */
 	public Integer getColumnOrder() {
 		return columnOrder;
 	}
 
+	/**
+	 * Sets the column order.
+	 *
+	 * @param columnOrder the column order
+	 */
 	public void setColumnOrder(Integer columnOrder) {
 		this.columnOrder = columnOrder;
 	}
 
+	/**
+	 * Gets the i2b2 concept this column is configured for.
+	 *
+	 * @return the {@link I2b2Concept} this column is configured for
+	 */
 	public I2b2Concept getI2b2Concept() {
 		return i2b2Concept;
 	}
 
+	/**
+	 * Sets the i2b2 concept.
+	 * @param i2b2Concept the i2b2 concept
+	 */
 	public void setI2b2Concept(I2b2Concept i2b2Concept) {
 		this.i2b2Concept = i2b2Concept;
 	}
 
+	/**
+	 * Gets the name of the column.
+	 *
+	 * @return the column name
+	 */
 	public String getColumnName() {
 		return columnName;
 	}
 
+	/**
+	 * Sets the column name.
+	 *
+	 * @param columnName the column name
+	 */
 	public void setColumnName(String columnName) {
 		this.columnName = columnName;
 	}
 
+	/**
+	 * Gets the display format of this column configuration, one of: EXISTENCE, VALUE, or
+	 *
+	 * @return the {@link DisplayFormat} used by this column configuration
+	 */
 	public DisplayFormat getDisplayFormat() {
 		return displayFormat;
 	}
 
+	/**
+	 * Sets the display format.
+	 *
+	 * @param displayFormat the display format
+	 */
 	public void setDisplayFormat(
 			DisplayFormat displayFormat) {
 		this.displayFormat = displayFormat;
 	}
 
+	/**
+	 * Gets how many repetitions of this column to output.
+	 *
+	 * @return how many repetitions of this column to output
+	 */
 	public Integer getHowMany() {
 		return howMany;
 	}
 
+	/**
+	 * Sets how many repetitions of this column to output if the display format is {@link DisplayFormat#VALUE}.
+	 *
+	 * @param howMany how many
+	 */
 	public void setHowMany(Integer howMany) {
 		this.howMany = howMany;
 	}
 
+	/**
+	 * Gets whether or not to include units if the display format is {@link DisplayFormat#VALUE}
+	 * or {@link DisplayFormat#AGGREGATION}.
+	 *
+	 * @return whether or not to include units
+	 */
 	public Boolean getIncludeUnits() {
 		return includeUnits;
 	}
 
+	/**
+	 * Sets whether or not to include units.
+	 *
+	 * @param includeUnits whether or not to include units
+	 */
 	public void setIncludeUnits(Boolean includeUnits) {
 		this.includeUnits = includeUnits;
 	}
 
+	/**
+	 * Gets whether or not to include the time range if the display format is {@link DisplayFormat#VALUE}.
+	 *
+	 * @return whether or not to include time range
+	 */
 	public Boolean getIncludeTimeRange() {
 		return includeTimeRange;
 	}
 
+	/**
+	 * Sets whether or not to include the time range.
+	 *
+	 * @param includeTimeRange whether or not to include the time range
+	 */
 	public void setIncludeTimeRange(Boolean includeTimeRange) {
 		this.includeTimeRange = includeTimeRange;
 	}
 
+	/**
+	 * Gets the type of aggregation if the display format is {@link DisplayFormat#AGGREGATION}.
+	 *
+	 * @return the aggregation type
+	 */
 	public AggregationType getAggregation() {
 		return aggregation;
 	}
 
+	/**
+	 * Sets the aggregation type.
+	 *
+	 * @param aggregation the aggregation type
+	 */
 	public void setAggregation(
 			AggregationType aggregation) {
 		this.aggregation = aggregation;
--- i2b2-export-service/src/main/java/edu/emory/cci/aiw/i2b2export/entity/OutputConfiguration.java
+++ i2b2-export-service/src/main/java/edu/emory/cci/aiw/i2b2export/entity/OutputConfiguration.java
@@ -35,7 +35,9 @@ import javax.persistence.UniqueConstraint;
 import java.util.List;
 
 /**
+ * A JPA entity for representing output configurations.
  *
+ * @author Michel Mansour
  */
 @Entity
 @Table(name = "output_configs", uniqueConstraints = {
@@ -56,8 +58,24 @@ public class OutputConfiguration {
 	@Column(name = "config_name")
 	private String name;
 
+	/**
+	 * Valid row dimensin values
+	 */
 	public static enum RowDimension {
-		PATIENT, VISIT, PROVIDER;
+		/**
+		 * each row represents a patient
+		 */
+		PATIENT,
+
+		/**
+		 * each row represents a visit
+		 */
+		VISIT,
+
+		/**
+		 * each row represents a provider
+		 */
+		PROVIDER;
 	}
 
 	@Column(name = "row_dimension", nullable = false)
@@ -78,77 +96,165 @@ public class OutputConfiguration {
 	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "outputConfig")
 	private List<OutputColumnConfiguration> columnConfigs;
 
+	/**
+	 * Gets the ID of the output configuration record.
+	 *
+	 * @return the ID
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the ID of the output configuration record.
+	 *
+	 * @param id the ID
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	/**
+	 * Gets the username of the user associated with this configuration.
+	 *
+	 * @return the username
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Sets the username
+	 *
+	 * @param username the username
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * Gets the name of the configuration
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name of the configuration
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Gets the row dimension.
+	 *
+	 * @return the row dimension
+	 */
 	public RowDimension getRowDimension() {
 		return rowDimension;
 	}
 
+	/**
+	 * Sets the row dimension.
+	 *
+	 * @param rowDimension the row dimension
+	 */
 	public void setRowDimension(RowDimension rowDimension) {
 		this.rowDimension = rowDimension;
 	}
 
+	/**
+	 * Gets the header row whitespace replacement string.
+	 *
+	 * @return the whitespace replacement string
+	 */
 	public String getWhitespaceReplacement() {
 		return whitespaceReplacement;
 	}
 
+	/**
+	 * Sets the whitespace replacement string.
+	 *
+	 * @param whitespaceReplacement the whitespace replacement string
+	 */
 	public void setWhitespaceReplacement(String whitespaceReplacement) {
 		this.whitespaceReplacement = whitespaceReplacement;
 	}
 
+	/**
+	 * Gets the column separator/delimiter.
+	 *
+	 * @return the separator
+	 */
 	public String getSeparator() {
 		return separator;
 	}
 
+	/**
+	 * Sets the separator.
+	 *
+	 * @param separator the separator
+	 */
 	public void setSeparator(String separator) {
 		this.separator = separator;
 	}
 
+	/**
+	 * Gets the character used to quote the data.
+	 *
+	 * @return the quote character
+	 */
 	public String getQuoteChar() {
 		return quoteChar;
 	}
 
+	/**
+	 * Sets the quote character
+	 *
+	 * @param quoteChar the quote character
+	 */
 	public void setQuoteChar(String quoteChar) {
 		this.quoteChar = quoteChar;
 	}
 
+	/**
+	 * Gets the string to use when data is missing for a column.
+	 *
+	 * @return the missing value string
+	 */
 	public String getMissingValue() {
 		return missingValue;
 	}
 
+	/**
+	 * Sets the missing value string.
+	 *
+	 * @param missingValue the missing value string
+	 */
 	public void setMissingValue(String missingValue) {
 		this.missingValue = missingValue;
 	}
 
+	/**
+	 * Gets the list of output column configurations that make up this output configuration.
+	 *
+	 * @return a list of {@link OutputColumnConfiguration}s
+	 */
 	@JsonManagedReference
 	public List<OutputColumnConfiguration> getColumnConfigs() {
 		return columnConfigs;
 	}
 
-	public void setColumnConfigs(
-			List<OutputColumnConfiguration> columnConfigs) {
+	/**
+	 * Sets the list of output column configurations.
+	 *
+	 * @param columnConfigs the output column configurations
+	 */
+	public void setColumnConfigs(List<OutputColumnConfiguration> columnConfigs) {
 		this.columnConfigs = columnConfigs;
 	}
 }
